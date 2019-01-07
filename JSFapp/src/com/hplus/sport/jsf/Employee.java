45ca0c483b0d85f043a12ebfc29704b8049e36b2
abrar zahin <abrarzahin@yahoo.com>
2019-01-06 23:54:06 -0500

    3 last

--- JSFapp/src/com/hplus/sport/jsf/Employee.java
+++ JSFapp/src/com/hplus/sport/jsf/Employee.java
@@ -9,81 +9,91 @@ public class Employee {
 	private String degree;
 	private String title;
 	private String photo;
-	private String descreption;
+	private String description;
 	
+	private String fullName;
 	
+	/*
+	 * Constructor
+	 */
 	public Employee() {
 		
 	}
 
-
+	
+	/*
+	 * Setters and Getters
+	 */
 	public int getID() {
 		return ID;
 	}
-
 
 	public void setID(int iD) {
 		ID = iD;
 	}
 
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-
 	public String getLastName() {
 		return lastName;
 	}
-
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
-
 	public String getDegree() {
 		return degree;
 	}
-
 
 	public void setDegree(String degree) {
 		this.degree = degree;
 	}
 
-
 	public String getTitle() {
 		return title;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
 	public String getPhoto() {
 		return photo;
 	}
-
 
 	public void setPhoto(String photo) {
 		this.photo = photo;
 	}
 
+	public String getDescription() {
+		return description;
+	}
 
-	public String getDescreption() {
-		return descreption;
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 
-	public void setDescreption(String descreption) {
-		this.descreption = descreption;
+	public String getFullName() {
+		String fname = firstName + " " + lastName;
+		if(!degree.isEmpty()) {
+			fname += ", " + degree;
+		}
+		return fname;
 	}
+
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	
 
 }
